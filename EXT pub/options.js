881fec775e7eebeee97951f9ba0390f112a949c0
A. wolo Wolski <wolo.wolski@gmail.com>
2018-06-25 00:21:48 +0200

    projects stored in separated items (due to single item size limit)

--- EXT pub/options.js
+++ EXT pub/options.js
@@ -26,13 +26,12 @@ var ExtOptions = {
      */
     optionsSave : function() {
 
-        var env_projects = ExtOptions.collectEnvSettings();
+        var projects = ExtOptions.collectEnvSettings();
     
         chrome.storage.sync.set({
     
             'switch_fe_openSelectedPageUid' :   $( '#switch_fe_openSelectedPageUid' ).is( ':checked' ),
             'switch_be_useBaseHref' :           $( '#switch_be_useBaseHref' ).is( ':checked' ),
-            'env_projects' :                    env_projects,
             'env_enable' :                      $( '#env_enable' ).is( ':checked' ),
             'env_switching' :                   $( '#env_switching' ).is( ':checked' ),
             'env_badge' :                       $( '#env_badge' ).is( ':checked' ),
@@ -42,18 +41,44 @@ var ExtOptions = {
             'ext_debug' :                       $( '#ext_debug' ).is( ':checked' )
 
         }, function() {
-            // update storage info
-            ExtOptions.updateStorageInfo();
-            ExtOptions.debugStorageData();
-            ExtOptions.fillExportData( env_projects );
-            // update status message
+
+            // in case of problems show info and end operation
             if (chrome.runtime.lastError)   {
                 ExtOptions.displayMessage( 'Options save problem -  ' + chrome.runtime.lastError.message, null, 100000 );
             }
+            // if options saved ok, now save projects
             else    {
-                ExtOptions.displayMessage( 'Options saved.' );
-                // reload extension to reapply settings
-                chrome.extension.getBackgroundPage().window.location.reload();
+
+                chrome.storage.sync.set(
+
+                    projects
+
+                , function() {
+                    // update storage info
+                    ExtOptions.updateStorageInfo();
+                    ExtOptions.debugStorageData();
+                    ExtOptions.fillExportData( projects );
+                    // update status message and show error if any
+                    if (chrome.runtime.lastError)   {
+                        ExtOptions.displayMessage( 'Options save problem -  ' + chrome.runtime.lastError.message, null, 100000 );
+                    }
+                    else    {
+                        ExtOptions.displayMessage( 'Options saved.' );
+
+                        // store projects number
+                        chrome.storage.sync.set({
+                            'env_projects_count' : Object.keys(projects).length     // this is the way to read number of elements of an object (like array length)
+                            //'env_projects' :    // remove old-way saved projects from storage (uncomment in few versions, to cleanup. now leave for keeping backup)
+                        }, function() {
+                            //console.log('env_projects_count: ' + Object.keys(projects).length);
+                            // if settings + projects was saved successfully, we can assume this was too. so no need to check again
+                            // reload extension to reapply settings
+                            chrome.extension.getBackgroundPage().window.location.reload();
+                        });
+
+                    }
+                });
+
             }
         });
     },
@@ -69,7 +94,8 @@ var ExtOptions = {
             // Set default values on read if not found
             'switch_fe_openSelectedPageUid' :   true,
             'switch_be_useBaseHref' :           true,
-            'env_projects' :                    [],
+            'env_projects' :                    [],     // leave for compatibility - must try to read old projects array to migrate
+            'env_projects_count' :              0,      // new project store way saves projects counter, so it means it's after migration
             'env_enable' :                      true,
             'env_switching' :                   true,
             'env_badge' :                       true,
@@ -94,8 +120,28 @@ var ExtOptions = {
             ExtOptions.DEV = options.ext_debug;
             ExtOptions.options = options;
 
-            ExtOptions.populateEnvSettings(                         options.env_projects );
-            ExtOptions.fillExportData( options.env_projects );
+            // if count is saved, it means the separated projects save method is used / after migration
+            if (options.env_projects_count) {
+                // read all options and extract projects
+                // read them separately from above, to keep possibility to set defaults on read
+                chrome.storage.sync.get(null, function(allOptions)    {
+                    var i;
+                    var projects = [];
+                    for (i = 0; i < options.env_projects_count; i++)    {
+                        projects.push(allOptions['proj_'+i]);
+                    }
+
+                    ExtOptions.populateEnvSettings( projects );
+                    ExtOptions.fillExportData( projects );
+                });
+            }
+            else    {
+                ExtOptions.populateEnvSettings( options.env_projects );
+                ExtOptions.fillExportData( options.env_projects );
+            }
+
+// todo wazne: zobaczyc jak to bedzie odczytywane w background.js w normalnym uzyciu
+
             ExtOptions.debugStorageData();
         });
     },
@@ -312,7 +358,8 @@ var ExtOptions = {
      * Iterate projects / environments elements and build an array
      */
     collectEnvSettings : function()   {
-        var projects = [];
+        var projects = {};
+        var counter = 0;
         $( '.settings-block.environments .projects-container .projectItem' ).each( function()  {
             var projectItem = {};
             projectItem['name'] = $(this).find( "[name='project[name]']" ).val();
@@ -339,7 +386,9 @@ var ExtOptions = {
                 projectItem['links'].push( linkItem );
             });
 
-            projects.push( projectItem );
+            projects['proj_'+counter] = projectItem;
+            // projects.push( projectItem );
+            counter++;
         });
         console.info('collectEnvSettings - projects: ', projects);
         return projects;
