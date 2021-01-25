6d294e6334571400a218e887afe0e6c013a41185
Paweł <p.schwalkowski@gmail.com>
2021-01-25 20:00:29 +0100

    prevent warning if config file is not exists

--- projects_repository/index.php
+++ projects_repository/index.php
@@ -46,14 +46,24 @@
  */
 class ProjectsRepository {
 
+    /**
+     * @var string 
+     */
     protected $dataDir = 'data';
-    protected $config = [];
 
+    /**
+     * @var array
+     */
+    protected $config = [];
 
     public function __construct() {
         // include optional config
         if (file_exists('config/config.php')) {
             $this->config = @include_once('config/config.php');
+        } else {
+            $this->config = [
+                'repo_key' => '',
+            ];
         }
     }
 
